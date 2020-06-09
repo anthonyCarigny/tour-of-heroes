8311f663fa5df5401f66471738b2b820e30a08cd
anthony <anthony.carigny@gmail.com>
2020-06-09 01:29:24 +0100

    putting avengers on the dashboard

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -12,13 +12,26 @@ import { MarvelResponse } from './marvel-response';
   providedIn: 'root',
 })
 export class HeroService {
-  private PUBLIC_KEY = "---";
+  private PUBLIC_KEY = "public_key_goes_here";
   private heroesUrl = `https://gateway.marvel.com:443/v1/public`; // URL to web api
 
   constructor(
     private http: HttpClient,
     private messageService: MessageService
   ) {}
+
+  getAvengers(): Observable<Hero[]> {
+    const url = `${this.heroesUrl}/series/24229/characters?&apikey=${this.PUBLIC_KEY}`;
+    // TODO: send the message _after_ fetching the heroes
+    return this.http.get<MarvelResponse>(url)
+    .pipe(
+      map(marvelResponse => marvelResponse.data.results),
+      tap(h => {
+        const outcome = h ? `fetched` : `did not find`;
+        this.log(`${outcome}`);
+      })
+    );
+  }
 
   getHeroes(): Observable<Hero[]> {
     const url = `${this.heroesUrl}/characters?apikey=${this.PUBLIC_KEY}`;
@@ -41,6 +54,7 @@ export class HeroService {
       map(marvelResponse => marvelResponse.data.results[0]),
       tap(h => {
         const outcome = h ? `fetched` : `did not find`;
+        console.log(h)
         this.log(`HeroService: ${outcome} hero id=${id}`);
       })
     );
